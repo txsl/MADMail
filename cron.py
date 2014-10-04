e3ec15e80d1443bf96d27159d51e38102468275c
Thomas Lim <txl11@ic.ac.uk>
2014-10-04 11:39:23 +0100

    Things are behaving pretty well now -sending nicely and merging good (phew!)

--- cron.py
+++ cron.py
@@ -17,6 +17,7 @@ MERGE_MAPPINGS = {
     "!CHILDEMAILS!": "ChildEmails",
 }
 
+TEMPLATE = 'email.html'
 
 emails_to_send = return_emails_to_send()
 
@@ -37,25 +38,28 @@ for email in emails_to_send:
             merged_content = merged_content.replace(tag, family_names[parents][data])
 
         send_to = []
+
         if email.email_parents:
             send_to = family_emails[parents]['Parents']
-        if email.email_parents:
-            send_to = family_emails[parents]['Children']
+        if email.email_children:
+            send_to = send_to + family_emails[parents]['Children']
 
-        # Uncomment when deployed hashtag safetyfirst hashtag alwaysuseprotection
+        print email.email_parents, email.email_children
+        print send_to
+
+        # Comment when deployed hashtag safetyfirst hashtag alwaysuseprotection
         send_to = ['txl11@ic.ac.uk']
 
         # Time for templating
-        template = env.get_template('different_email.html')
-        output = template.render(content=Markup(merged_content))
+        template = env.get_template(TEMPLATE)
+        output = template.render(content=Markup(merged_content), top_text=email.top_text)
         text_output = strip_tags(merged_content)
 
 
         send_mail(email.Subject, ("Mums and Dads", "mumsanddads@imperial.ac.uk"),
                               send_to, html_body=output, text_body=text_output)
 
-        time.sleep(.5)
-    # mail.send_queue()
+    mail.send_queue()
 
 
 """
